f4624b41135aa75dfef4728b2d6cc5a288965609
Jonathan Berry <jonathan.s.berry@googlemail.com>
2020-10-06 20:26:53 +0100

    Fix flake8 errors.

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 from setuptools import setup, find_packages
-import sys, os
 
 version = '0.0'
 
@@ -8,7 +7,7 @@ setup(
     version=version,
     description="Styling for UNAIDS",
     long_description="""""",
-    classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[],
     keywords='',
     author='Fjelltopp',
     author_email='',
@@ -18,9 +17,7 @@ setup(
     namespace_packages=['ckanext'],
     include_package_data=True,
     zip_safe=False,
-    install_requires=[
-	# -*- Extra requirements: -*-
-    ],
+    install_requires=[],
     entry_points="""
     [ckan.plugins]
     # Add plugins here, eg
