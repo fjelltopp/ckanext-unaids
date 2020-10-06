92867b99d1143b520e10483004cd5858df70b0c8
Jonathan Berry <jonathan.s.berry@googlemail.com>
2020-10-06 19:27:28 +0100

    ADX-281 Regression test.

--- ckanext/unaids/tests/test_plugin.py
+++ ckanext/unaids/tests/test_plugin.py
@@ -4,31 +4,30 @@
 # from nose.tools import assert_raises
 import ckan.model as model
 import ckan.plugins
+from ckan.tests.helpers import call_action, reset_db
+from ckan.lib.helpers import url_for
+from ckan.tests import factories
+import ckan.tests.helpers as helpers
+from ckanext.validation.model import create_tables, tables_exist
+import logging
 # import ckan.tests.factories as factories
 # import ckan.logic as logic
 
 
-class TestPlugin(object):
+class TestPlugin(helpers.FunctionalTestBase):
     '''Tests for the ckanext.example_iauthfunctions.plugin module.
 
     Specifically tests that overriding parent auth functions will cause
     child auth functions to use the overridden version.
     '''
-    @classmethod
-    def setup_class(cls):
-        '''Nose runs this method once to setup our test class.'''
-        # Test code should use CKAN's plugins.load() function to load plugins
-        # to be tested.
+    def setup(self):
+        reset_db()
+        if not tables_exist():
+            create_tables()
+        self.app = self._get_test_app()
         ckan.plugins.load('unaids')
 
     def teardown(self):
-        '''Nose runs this method after each test method in our test class.'''
-        # Rebuild CKAN's database after each test method, so that each test
-        # method runs with a clean slate.
-        model.repo.rebuild_db()
-
-    @classmethod
-    def teardown_class(cls):
         '''
         Nose runs this method once after all the test methods in our class
         have been run.
@@ -39,6 +38,17 @@ class TestPlugin(object):
 
     def test_geojson_format_guessed_correctly(self):
         '''
-        Placeholder test.
+        Test that the format of a geojson file is guessed correctly.
         '''
-        pass
+        logging.warning('RUNNING Test')
+        dataset = factories.Dataset()
+        resource = {
+            'name': 'test',
+            'url': 'file.geojson',
+            'package_id': dataset['id'],
+            'format': '',
+            'id': ''
+        }
+        response = call_action('resource_create', {}, **resource)
+        response = call_action('package_show', {}, id=dataset['id'])
+        assert response['resources'][0]['format'] == 'GeoJSON'
