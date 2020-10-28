8de82220c80869846c1cb855bfcb7961dd386a6a
Jonathan Berry <jonathan.s.berry@googlemail.com>
2020-10-28 21:38:08 +0000

    ADX-458 Upgrade ckanext-unaids tests to run against CKAN 2.9

--- bin/travis-run.sh
+++ bin/travis-run.sh
@@ -1,19 +1,4 @@
-#!/bin/sh -e
-set -ex
+#!/bin/bash
+set -e
 
-flake8 --version
-# stop the build if there are Python syntax errors or undefined names
-flake8 . --count --select=E901,E999,F821,F822,F823 --show-source --statistics --exclude ckan
-
-nosetests --ckan \
-          --nologcapture \
-          --with-pylons=subdir/test.ini \
-          --with-coverage \
-          --cover-package=ckanext.unaids \
-          --cover-inclusive \
-          --cover-erase \
-          --cover-tests \
-          ckanext/unaids
-
-# strict linting
-flake8 . --count --max-line-length=127 --statistics --exclude ckan,src
+pytest --ckan-ini=subdir/test.ini --cov=ckanext.unaids ckanext/unaids/tests
