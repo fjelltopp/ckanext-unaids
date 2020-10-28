412c65f338375c03f10e8d584381ca3bd9d99015
Tomasz Sabała <sabala.tomek@gmail.com>
2020-10-28 21:01:34 +0100

    ADX-456 /me endpoint display api key the same way as new CKAN apikey UI.

--- ckanext/unaids/blueprints/user_info_blueprint.py
+++ ckanext/unaids/blueprints/user_info_blueprint.py
@@ -1,9 +1,11 @@
 # encoding: utf-8
 import logging
-from ckan.common import g
+from ckan.common import g, _
 from ckan.lib import helpers as h
 from ckan.logic import get_action
 from ckan.views.user import before_request
+from six import ensure_str
+import dominate.tags as dom_tags
 from flask import Blueprint
 
 log = logging.getLogger(__name__)
@@ -25,7 +27,21 @@ def display_user_details(locale=None):
         data_dict = {'id': g.user}
 
         user_dict = get_action('user_show')(context, data_dict)
-        h.flash_success("Your api key is: {}".format(user_dict['apikey']))
+        apikey = user_dict['apikey']
+        copy_btn = dom_tags.button(dom_tags.i(u'', {
+            u'class': u'fa fa-copy'
+        }), {
+            u'type': u'button',
+            u'class': u'btn btn-default btn-xs',
+            u'data-module': u'copy-into-buffer',
+            u'data-module-copy-value': ensure_str(apikey)
+       })
+        h.flash_success(
+            _('Your api key is: '
+              '<code style=\"word-break:break-all;\"> {token}</code> {copy}'
+            ).format(token=apikey, copy=copy_btn),
+            allow_html=True
+        )
         return h.redirect_to(locale=locale, controller='user', action='read', id=g.user)
 
 
