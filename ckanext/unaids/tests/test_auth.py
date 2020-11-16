ba8a4d9507500dee34d8b14c80c8dd6da9ec05a3
manoj-nathwani <manoj_1993@hotmail.com>
2020-11-16 17:29:34 +0000

    added auth test

---
+++ ckanext/unaids/tests/test_auth.py
@@ -0,0 +1,35 @@
+'Tests for plugin.py.'
+# encoding: utf-8
+
+from ckan.tests import factories
+from ckanext.unaids.auth import (
+    unaids_organization_update
+)
+import pytest
+import logging
+
+log = logging.getLogger(__name__)
+
+
+@pytest.mark.ckan_config('ckan.plugins', 'unaids scheming_datasets')
+@pytest.mark.usefixtures('with_plugins')
+@pytest.mark.usefixtures('clean_db')
+class TestAuth(object):
+
+    def test_unaids_organization_update_valid(self, app):
+        user = factories.User()
+        org = factories.Organization(user=user)
+        response = unaids_organization_update(
+            {'user': user['name']},
+            {'id': org['id']}
+        )
+        assert response['success']
+
+    def test_unaids_organization_update_invalid(self, app):
+        user = factories.User()
+        org = factories.Organization()
+        response = unaids_organization_update(
+            {'user': user['name']},
+            {'id': org['id']}
+        )
+        assert not response['success']
