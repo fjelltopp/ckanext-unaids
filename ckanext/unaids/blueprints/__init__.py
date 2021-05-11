30e4d4a214c201037e035439737cf3d27c914e50
manoj-nathwani <manoj_1993@hotmail.com>
2021-05-11 16:31:35 +0000

    first push of frontend

--- ckanext/unaids/blueprints/__init__.py
+++ ckanext/unaids/blueprints/__init__.py
@@ -1,4 +1,11 @@
 from ckanext.unaids.blueprints.unaids_blueprint import unaids_blueprint
 from ckanext.unaids.blueprints.unaids_dataset_transfer import unaids_dataset_transfer
 from ckanext.unaids.blueprints.user_info_blueprint import user_info_blueprint
-blueprints = [unaids_blueprint, unaids_dataset_transfer, user_info_blueprint]
+from ckanext.unaids.blueprints.unaids_dataset_releases import unaids_dataset_releases
+
+blueprints = [
+    unaids_blueprint,
+    unaids_dataset_transfer,
+    user_info_blueprint,
+    unaids_dataset_releases
+]
