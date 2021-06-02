72fdc2e472da3922b1275903e263b5ee99b57bad
manoj-nathwani <manoj_1993@hotmail.com>
2021-06-02 16:07:07 +0000

    finished feature

--- ckanext/unaids/blueprints/unaids_dataset_releases.py
+++ ckanext/unaids/blueprints/unaids_dataset_releases.py
@@ -11,9 +11,12 @@ unaids_dataset_releases = Blueprint(
     u'unaids_dataset_releases',
     __name__
 )
+SOMETHING_WENT_WRONG_ERROR = toolkit._(
+    'Something went wrong. Please try again.')
 
 
 def _get_context():
+    # TODO: confirm all of these fields are needed
     return {
         'model': model,
         'session': model.Session,
@@ -23,12 +26,14 @@ def _get_context():
     }
 
 
-def list_releases(dataset_type, id):
+def list_releases(dataset_type, dataset_id):
+    # TODO: confirm this shouldn't be a MethodView Class
+    # as we're also referring to it from read_base.html
     dataset = toolkit.get_action('package_show')(
-        _get_context(), {'id': id}
+        _get_context(), {'id': dataset_id}
     )
     releases = toolkit.get_action('dataset_version_list')(
-        _get_context(), {'dataset_id': id}
+        _get_context(), {'dataset_id': dataset_id}
     )
     return toolkit.render(
         'package/releases/list.html',
@@ -39,7 +44,7 @@ def list_releases(dataset_type, id):
     )
 
 
-def _dataset_release_view(dataset_id, template):
+def _get_dataset_and_release(dataset_id):
     dataset = toolkit.get_action('package_show')(
         _get_context(), {'id': dataset_id}
     )
@@ -50,117 +55,141 @@ def _dataset_release_view(dataset_id, template):
         )
     else:
         release = None
+    return dataset, release
+
+
+def _dataset_release_view(dataset_id, template):
+    dataset, release = _get_dataset_and_release(dataset_id)
     return toolkit.render(
-        'package/releases/{}'.format(template),
+        'package/releases/{}.html'.format(template),
         {'pkg_dict': dataset, 'release': release}
     )
 
 
 class ReleaseView(MethodView):
-    def get(self, dataset_type, id):
+    def get(self, dataset_type, dataset_id):
         return _dataset_release_view(
-            id, 'create_or_edit.html'
+            dataset_id, 'create_or_edit'
         )
 
-    def post(self, dataset_type, id):
+    def post(self, dataset_type, dataset_id):
         dataset = toolkit.get_action('package_show')(
-            _get_context(), {'id': id}
+            _get_context(), {'id': dataset_id}
         )
+        # TODO: replace form with ckan form
         name = request.form['name']
         notes = request.form.get('notes')
-        if 'release_id' in request.args:
-            release_id = request.args['release_id']
-            release = toolkit.get_action('version_update')(
-                _get_context(),
-                {
-                    'package_id': id,
-                    'version_id': release_id,
-                    'name': name,
-                    'notes': notes
-                })
-            h.flash_success(
-                _('Release {} updated').format(release['name'])
-            )
+        release_id = request.args.get('release_id')
+        activity_id = request.args.get('activity_id')
+        try:
+            if release_id:
+                release = toolkit.get_action('version_update')(
+                    _get_context(),
+                    {
+                        'package_id': dataset_id,
+                        'version_id': release_id,
+                        'name': name,
+                        'notes': notes
+                    })
+            else:
+                release = toolkit.get_action('dataset_version_create')(
+                    _get_context(),
+                    {
+                        'dataset_id': dataset['id'],
+                        'activity_id': activity_id,
+                        'name': name,
+                        'notes': notes
+                    })
+        except toolkit.ValidationError:
+            h.flash_error(toolkit._(
+                'A release with the name {} already exists.'.format(name)
+            ))
+            return h.redirect_to(request.url)
+        except Exception:
+            h.flash_error(SOMETHING_WENT_WRONG_ERROR)
+            return h.redirect_to(request.url)
         else:
-            activity_id = request.args.get('activity_id')
-            release = toolkit.get_action('dataset_version_create')(
-                _get_context(),
-                {
-                    'dataset_id': dataset['id'],
-                    'activity_id': activity_id,
-                    'name': name,
-                    'notes': notes
-                }
-            )
-            h.flash_success(
-                _('Release {} added').format(release['name'])
-            )
+            flash_message = _('Release {} updated').format(release['name']) \
+                if release else _('Release {} added').format(release['name'])
+            h.flash_success(flash_message)
+        # TODO: update redirect so activity_id isn't removed
         return h.redirect_to(
             controller='dataset',
             action='read',
-            id=dataset['name'],
-            release_id=release['id']
+            id=dataset['id'],
+            activity_id=activity_id
         )
 
 
 class ReleaseDelete(MethodView):
-    def get(self, dataset_type, id):
-        return _dataset_release_view(id, 'delete.html')
+    def get(self, dataset_type, dataset_id):
+        return _dataset_release_view(
+            dataset_id, 'delete'
+        )
 
-    def post(self, dataset_type, id):
-        dataset = toolkit.get_action('package_show')(
-            _get_context(), {'id': id}
-        )
-        release_id = request.args['release_id']
-        release = toolkit.get_action('version_delete')(
-            _get_context(), {'version_id': release_id}
-        )
-        h.flash_success(
-            _('Release {} deleted').format(release['name'])
-        )
-        return h.redirect_to(
-            controller='dataset',
-            action='read',
-            id=dataset['name']
-        )
+    def post(self, dataset_type, dataset_id):
+        dataset, release = _get_dataset_and_release(dataset_id)
+        try:
+            toolkit.get_action('version_delete')(
+                _get_context(), {'version_id': release['id']}
+            )
+        except toolkit.ObjectNotFound:
+            h.flash_error(SOMETHING_WENT_WRONG_ERROR)
+            return h.redirect_to(request.url)
+        else:
+            h.flash_success(
+                _('Release {} deleted').format(release['name'])
+            )
+            return h.redirect_to(
+                controller='dataset',
+                action='read',
+                id=dataset['name']
+            )
 
 
 class ReleaseRestore(MethodView):
-    def get(self, dataset_type, id):
-        return _dataset_release_view(id, 'restore.html')
+    def get(self, dataset_type, dataset_id):
+        return _dataset_release_view(
+            dataset_id, 'restore'
+        )
 
-    def post(self, dataset_type, id):
-        dataset = toolkit.get_action('package_show')(
-            _get_context(), {'id': id}
-        )
-        release_id = request.args['release_id']
-        release = toolkit.get_action('version_show')(
-            _get_context(), {'version_id': release_id}
-        )
-        h.flash_success(
-            _('Release {} restored').format(release['name'])
-        )
-        return h.redirect_to(
-            controller='dataset',
-            action='read',
-            id=dataset['name'],
-            release_id=release['id']
-        )
+    def post(self, dataset_type, dataset_id):
+        dataset, release = _get_dataset_and_release(dataset_id)
+        try:
+            toolkit.get_action('dataset_version_restore')(
+                _get_context(), {
+                    'dataset_id': dataset['id'],
+                    'version_id': release['id']
+                }
+            )
+        except toolkit.ObjectNotFound:
+            h.flash_error(SOMETHING_WENT_WRONG_ERROR)
+            return h.redirect_to(request.url)
+        else:
+            h.flash_success(
+                _('Release {} restored').format(release['name'])
+            )
+            return h.redirect_to(
+                controller='dataset',
+                action='read',
+                id=dataset['name'],
+                release_id=release['id']
+            )
 
 
 unaids_dataset_releases.add_url_rule(
-    u'/<dataset_type>/<id>/releases',
+    u'/<dataset_type>/<dataset_id>/releases',
     view_func=list_releases
 )
 unaids_dataset_releases.add_url_rule(
-    u'/<dataset_type>/<id>/release',
+    u'/<dataset_type>/<dataset_id>/release',
     view_func=ReleaseView.as_view('release_view')
 )
 unaids_dataset_releases.add_url_rule(
-    u'/<dataset_type>/<id>/release/delete',
+    u'/<dataset_type>/<dataset_id>/release/delete',
     view_func=ReleaseDelete.as_view('release_delete')
 )
 unaids_dataset_releases.add_url_rule(
-    u'/<dataset_type>/<id>/release/restore',
+    u'/<dataset_type>/<dataset_id>/release/restore',
     view_func=ReleaseRestore.as_view('release_restore')
 )
