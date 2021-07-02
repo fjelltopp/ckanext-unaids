a01568ee320d30d3aed68d56e85a68969ce0d9bc
manoj-nathwani <manoj_1993@hotmail.com>
2021-07-02 14:27:54 +0000

    added update_resource_last_modified_date to backend

--- ckanext/unaids/tests/test_plugin.py
+++ ckanext/unaids/tests/test_plugin.py
@@ -4,6 +4,9 @@
 from ckan.tests.helpers import call_action
 from ckan.tests import factories
 import pytest
+from ckanext.unaids.plugin import (
+    _update_resource_last_modified_date
+)
 
 
 @pytest.mark.ckan_config('ckan.plugins', 'unaids')
@@ -30,3 +33,61 @@ class TestPlugin(object):
         response = call_action('resource_create', {}, **resource)
         response = call_action('package_show', {}, id=dataset['id'])
         assert response['resources'][0]['format'] == 'GeoJSON'
+
+
+class TestResourceLastModified(object):
+    '''Tests for the ckanext.plugin._update_resource_last_modified_date module.
+
+    Make sure we are setting the last_modified to the resource dict
+    whenever a resource is created or edited.
+    '''
+
+    URL_TYPE_FILE_UPLOAD = 'upload'
+    URL_TYPE_LINK = ''
+
+    def test_resource(self, url_type):
+        resource_dict = {'url_type': url_type}
+        if url_type == self.URL_TYPE_FILE_UPLOAD:
+            resource_dict.update({
+                'url': 'file.csv',
+                'lfs_prefix': 'prefix123',
+                'sha256': 'sha256',
+                'size': 100,
+            })
+        elif url_type == self.URL_TYPE_LINK:
+            resource_dict.update({
+                'url': 'http://example.com'
+            })
+        return resource_dict
+
+    def test_null_to_file(self):
+        resource = self.test_resource(self.URL_TYPE_FILE_UPLOAD)
+        _update_resource_last_modified_date(resource=resource)
+        assert 'last_modified' in resource
+
+    def test_file_to_null(self):
+        resource = self.test_resource(self.URL_TYPE_FILE_UPLOAD)
+        _update_resource_last_modified_date({}, resource)
+        assert 'last_modified' not in resource
+
+    def test_file_to_url(self):
+        current = self.test_resource(self.URL_TYPE_FILE_UPLOAD)
+        resource = self.test_resource(self.URL_TYPE_LINK)
+        _update_resource_last_modified_date(resource, current)
+        assert 'last_modified' in resource
+
+    def test_null_to_url(self):
+        resource = self.test_resource(self.URL_TYPE_LINK)
+        _update_resource_last_modified_date(resource=resource)
+        assert 'last_modified' in resource
+
+    def test_url_to_null(self):
+        resource = self.test_resource(self.URL_TYPE_LINK)
+        _update_resource_last_modified_date({}, resource)
+        assert 'last_modified' not in resource
+
+    def test_url_to_file(self):
+        current = self.test_resource(self.URL_TYPE_LINK)
+        resource = self.test_resource(self.URL_TYPE_FILE_UPLOAD)
+        _update_resource_last_modified_date(resource, current)
+        assert 'last_modified' in resource
