9ec59402a469aea0db5ed0cfdf17f19b21167626
manoj-nathwani <manoj_1993@hotmail.com>
2021-07-05 13:06:16 +0000

    added more fixtures

--- ckanext/unaids/tests/test_plugin.py
+++ ckanext/unaids/tests/test_plugin.py
@@ -9,6 +9,53 @@ from ckanext.unaids.plugin import (
 )
 
 
+@pytest.fixture
+def resource_with_file():
+    return {
+        'url_type': 'upload',
+        'url': 'file.csv',
+        'lfs_prefix': 'prefix',
+        'sha256': 'sha256',
+        'size': 100,
+    }
+
+
+@pytest.fixture
+def resource_with_link():
+    return {
+        'url_type': '',
+        'url': 'http://example.com'
+    }
+
+
+@pytest.fixture
+def resource_with_updated_file():
+    updated_resource = resource_with_file()
+    updated_resource.update({'url': 'file2.csv'})
+    return updated_resource
+
+
+@pytest.fixture
+def resource_with_updated_link():
+    updated_resource = resource_with_link()
+    updated_resource.update({'url': 'http://example2.com'})
+    return updated_resource
+
+
+@pytest.fixture
+def resource_with_file_and_updated_metadata():
+    updated_resource = resource_with_file()
+    updated_resource.update({'key': 'value'})
+    return updated_resource
+
+
+@pytest.fixture
+def resource_with_link_and_updated_metadata():
+    updated_resource = resource_with_link()
+    updated_resource.update({'key': 'value'})
+    return updated_resource
+
+
 @pytest.mark.ckan_config('ckan.plugins', 'unaids')
 @pytest.mark.usefixtures('with_plugins')
 class TestPlugin(object):
@@ -42,23 +89,6 @@ class TestResourceLastModified(object):
     whenever a resource is created or edited.
     '''
 
-    @pytest.fixture
-    def resource_with_file(autouse=True):
-        return {
-            'url_type': 'upload',
-            'url': 'file.csv',
-            'lfs_prefix': 'prefix123',
-            'sha256': 'sha256',
-            'size': 100,
-        }
-
-    @pytest.fixture
-    def resource_with_link(autouse=True):
-        return {
-            'url_type': '',
-            'url': 'http://example.com'
-        }
-
     def test_null_to_file_should_update_last_modified_datetime(self, resource_with_file):
         _update_resource_last_modified_date(resource_with_file)
         assert 'last_modified' in resource_with_file
@@ -70,27 +100,26 @@ class TestResourceLastModified(object):
         )
         assert 'last_modified' not in resource_with_file
 
-    def test_file_to_file_should_update_last_modified_datetime(self, resource_with_file):
-        updated_resource = resource_with_file.copy()
-        updated_resource.update({'url': 'file2.csv'})
+    def test_file_to_file_should_update_last_modified_datetime(
+            self, resource_with_file, resource_with_updated_file):
         _update_resource_last_modified_date(
-            updated_resource, current=resource_with_file
+            resource_with_updated_file, current=resource_with_file
         )
-        assert 'last_modified' in updated_resource
+        assert 'last_modified' in resource_with_updated_file
 
-    def test_file_to_link_should_update_last_modified_datetime(self, resource_with_file, resource_with_link):
+    def test_file_to_link_should_update_last_modified_datetime(
+            self, resource_with_file, resource_with_link):
         _update_resource_last_modified_date(
             resource_with_link, current=resource_with_file
         )
         assert 'last_modified' in resource_with_link
 
-    def test_changing_metadata_in_file_should_not_update_last_modified_datetime(self, resource_with_file):
-        updated_resource = resource_with_file.copy()
-        updated_resource.update({'key': 'value'})
+    def test_changing_metadata_in_file_should_not_update_last_modified_datetime(
+            self, resource_with_file, resource_with_file_and_updated_metadata):
         _update_resource_last_modified_date(
-            resource_with_file, current=resource_with_file
+            resource_with_file_and_updated_metadata, current=resource_with_file
         )
-        assert 'last_modified' not in updated_resource
+        assert 'last_modified' not in resource_with_file_and_updated_metadata
 
     def test_null_to_link_should_update_last_modified_datetime(self, resource_with_link):
         _update_resource_last_modified_date(resource_with_link)
@@ -103,24 +132,23 @@ class TestResourceLastModified(object):
         )
         assert 'last_modified' not in resource_with_link
 
-    def test_link_to_link_should_update_last_modified_datetime(self, resource_with_link):
-        updated_resource = resource_with_link.copy()
-        updated_resource.update({'url': 'http://example2.com'})
+    def test_link_to_link_should_update_last_modified_datetime(
+            self, resource_with_link, resource_with_updated_link):
         _update_resource_last_modified_date(
-            updated_resource, current=resource_with_link
+            resource_with_updated_link, current=resource_with_link
         )
-        assert 'last_modified' in updated_resource
+        assert 'last_modified' in resource_with_updated_link
 
-    def test_link_to_file_should_update_last_modified_datetime(self, resource_with_file, resource_with_link):
+    def test_link_to_file_should_update_last_modified_datetime(
+            self, resource_with_file, resource_with_link):
         _update_resource_last_modified_date(
             resource_with_file, current=resource_with_link
         )
         assert 'last_modified' in resource_with_file
 
-    def test_changing_metadata_in_link_should_not_update_last_modified_datetime(self, resource_with_link):
-        updated_resource = resource_with_link.copy()
-        updated_resource.update({'key': 'value'})
+    def test_changing_metadata_in_link_should_not_update_last_modified_datetime(
+            self, resource_with_link, resource_with_link_and_updated_metadata):
         _update_resource_last_modified_date(
-            resource_with_link, current=resource_with_link
+            resource_with_link_and_updated_metadata, current=resource_with_link
         )
-        assert 'last_modified' not in updated_resource
+        assert 'last_modified' not in resource_with_link_and_updated_metadata
