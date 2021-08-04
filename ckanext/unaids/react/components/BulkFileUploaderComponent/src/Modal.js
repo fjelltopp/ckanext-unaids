a5ecd1bee1c7225ba9803d6eaf90536bb2fcf672
manoj-nathwani <manoj_1993@hotmail.com>
2021-08-04 09:24:17 +0000

    ADX-729-fix-modal-closing-config

--- ckanext/unaids/react/components/BulkFileUploaderComponent/src/Modal.js
+++ ckanext/unaids/react/components/BulkFileUploaderComponent/src/Modal.js
@@ -3,13 +3,17 @@ import ModalBody from './ModalBody';
 
 export default function Modal(props) {
     return (
-        <div className="modal fade" id={props.modalElementId}>
+        <div className="modal fade" id={props.modalElementId} data-backdrop="static" data-keyboard="false">
             <div className="modal-dialog" role="document">
                 <div className="modal-content">
                     <div className="modal-header">
-                        <button type="button" className="close" data-dismiss="modal">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
+                        {!props.uploadsComplete &&
+                            (
+                                <button type="button" className="close" data-dismiss="modal">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            )
+                        }
                         <h4 className="modal-title">{ckan.i18n._('Upload Resources')}</h4>
                     </div>
                     <div className="modal-body">
