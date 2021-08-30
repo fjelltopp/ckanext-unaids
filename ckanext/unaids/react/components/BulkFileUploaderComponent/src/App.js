2dc6cd1a9e467dcb9dd72284a174a560d216d18c
Jonathan Berry <jonathan.s.berry@googlemail.com>
2021-08-30 10:15:39 +0100

    ADX-725 Improved file matching.

--- ckanext/unaids/react/components/BulkFileUploaderComponent/src/App.js
+++ ckanext/unaids/react/components/BulkFileUploaderComponent/src/App.js
@@ -40,38 +40,65 @@ export default function App(props) {
     const [networkError, setNetworkError] = useState(false);
 
     const getDefaultUploadAction = file => {
-        // disabled until we figure out a nicer algorithm
-        // const fuzzySearchResults = new Fuse(
-        //     updateResourcesOptions,
-        //     {
-        //         // https://bit.ly/3BTLnWd
-        //         includeScore: true,
-        //         findAllMatches: false,
-        //         keys: ['fileName'],
-        //         threshold: 0.3
-        //     }
-        // ).search(file.name);
-        // let defaultUploadAction = extraResource;
-        // if (fuzzySearchResults.length === 1) {
-        //     defaultUploadAction = fuzzySearchResults[0].item;
-        // } else if (fuzzySearchResults.length > 1) {
-        //     const firstResult = fuzzySearchResults[0];
-        //     const secondResult = fuzzySearchResults[1];
-        //     const scroreDiff = firstResult.score - secondResult.score;
-        //     if (scroreDiff > 0.05) {
-        //         defaultUploadAction = fuzzySearchResults[0].item;
-        //     }
-        // }
-        // return defaultUploadAction;
-        const exactMatches = updateResourcesOptions.filter(
-            x => x.fileName === file.name
-        );
-        switch (exactMatches.length) {
-            case 1:
-                return exactMatches[0];
-            default:
-                return extraResource;
+
+        function strDiff(str1, str2){
+            // Get all chars that match at the start of both strings
+            var openingMatch = "";
+            for(let x = 0; x < str1.length; x++){
+                if(str1.charAt(x) == str2.charAt(x)) openingMatch += str1.charAt(x);
+                else break;
+            }
+
+            // Get all chars that match at the end of both strings
+            var str1Reversed = str1.split("").reverse().join("");
+            var str2Reversed = str2.split("").reverse().join("");
+            var endingMatch = "";
+            for(let x = 0; x < str1.length; x++){
+                if(str1Reversed.charAt(x) == str2Reversed.charAt(x)) endingMatch += str1Reversed.charAt(x);
+                else break;
+            }
+            endingMatch = endingMatch.split("").reverse().join("");
+
+            // Get the middle non-matching part of each string
+            var str1Difference = str1.slice(openingMatch.length, str1.length-endingMatch.length).trim();
+            var str2Difference = str2.slice(openingMatch.length, str2.length-endingMatch.length)
+
+            return [openingMatch, endingMatch, str1Difference, str2Difference];
         }
+
+        function filenameMatch(filename1, filename2){
+            /* Matches where filenames begin and end with the same three (or more chars) and
+               any middle non-matching parts consist only of the following inserted or swapped:
+               - a number
+               - a number in parenthesis
+               - a number after a hyphen
+            */
+            const regexp = /^$|^[\(-]? ?[0-9]*\)?$/;
+            const matches = strDiff(filename1, filename2);
+            const completeMatch = matches[0] == matches[1] && matches[0];
+            const openingMatch = matches[0].length >= 3;
+            const closingMatch = matches[1].length >= 3;
+            const regexMatch = matches[2].match(regexp) != null && matches[3].match(regexp) != null;
+            const match = completeMatch || openingMatch && closingMatch && regexMatch;
+            return match;
+        }
+
+        // Find all matched resources
+        var matchedResources = []
+        for(let x = 0; x<updateResourcesOptions.length; x++){
+            if(filenameMatch(file.name, updateResourcesOptions[x].fileName)){
+                matchedResources.push(updateResourcesOptions[x]);
+            }
+        }
+
+        // Set the default upload action only if there is a single matching resource
+        var defaultUploadAction = extraResource;
+        if(matchedResources.length == 1){
+            defaultUploadAction = matchedResources[0];
+        }
+
+        return defaultUploadAction;
+
     }
 
     const updatedProps = {
