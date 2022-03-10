321ec56d5a06332e6aa5e97d4ef47f5b150ca137
Jonathan S Berry <jonathan.s.berry@gmail.com>
2022-03-10 12:58:55 +0000

    Stub map_selector js module

---
+++ ckanext/unaids/assets/map_selector.js
@@ -0,0 +1,14 @@
+// Enable JavaScript's strict mode. Strict mode catches some common
+// programming errors and throws exceptions, prevents some unsafe actions from
+// being taken, and disables some confusing and bad JavaScript features.
+"use strict";
+
+this.ckan.module('map_selector', function($){
+	return {
+        options: {
+        },
+        initialize: function () {
+            console.log('map_selector.js initialised');
+        }
+    };
+});
