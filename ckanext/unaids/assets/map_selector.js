87fca47c863a66f9d15663a62b70d3e1f20a72b0
Jonathan S Berry <jonathan.s.berry@gmail.com>
2022-03-10 13:31:49 +0000

    Get svg map working with stub blueprint

--- ckanext/unaids/assets/map_selector.js
+++ ckanext/unaids/assets/map_selector.js
@@ -6,37 +6,19 @@
 this.ckan.module('map_selector', function ($) {
 	return {
         options: {
-        	configurl: "/api/3/action/unaids_map_config"
+        	configurl: "/map-options/"
         },
         initialize: function () {
-            new svgMap({
-            	targetElementID: this.el.attr('id'),
-            	data: this._getConfiguration()
-            });
-        },
-        _getConfiguration: function () {
-        	return  {
-			    data: {
-					gdp: {
-						name: "GDP per capita",
-						format: "{0} USD",
-						thousandSeparator: ',',
-						thresholdMax: 1,
-						thresholdMin: 0
-					},
-					change: {
-						name: 'Change to year before',
-						format: '{0} %'
-					}
-			    },
-			    touchLink: true,
-			    applyData: 'gdp',
-			    values: {
-					ES: {gdp: 0, change: 4.73, link: 'https://adr.unaids.org' },
-					AL: {gdp: 1, change: 11.09},
-					GB: {gdp: 1, change: 10.01}
-			    }
-			};
+        	var mapElementID = this.el.attr('id');
+        	$.getJSON({
+				url: this.options.configurl
+			}).done(function( data ) {
+				console.log(data);
+				new svgMap({
+	            	targetElementID: mapElementID,
+	            	data: data
+	            });
+			});
         }
     };
 });
