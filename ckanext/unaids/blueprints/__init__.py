d2ab9d566c85f15f5dbaff8262e922667342cc4e
Jonathan S Berry <jonathan.s.berry@gmail.com>
2022-08-17 15:06:20 +0100

    ADX-805 Remove autogeneration of resources

--- ckanext/unaids/blueprints/__init__.py
+++ ckanext/unaids/blueprints/__init__.py
@@ -1,11 +1,9 @@
 from ckanext.unaids.blueprints.svg_map_options import svg_map_options
-from ckanext.unaids.blueprints.unaids_blueprint import unaids_blueprint
 from ckanext.unaids.blueprints.unaids_dataset_transfer import unaids_dataset_transfer
 from ckanext.unaids.blueprints.user_info_blueprint import user_info_blueprint
 from ckanext.unaids.blueprints.unaids_dataset_releases import unaids_dataset_releases
 
 blueprints = [
-    unaids_blueprint,
     unaids_dataset_transfer,
     user_info_blueprint,
     unaids_dataset_releases,
