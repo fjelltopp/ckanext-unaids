5af52aaac078f4e62cd12aec11bd8c0adf5ab8d9
Jonathan S Berry <jonathan.s.berry@gmail.com>
2022-11-19 11:55:08 +0000

    ADX-849 Data out of date badge formatting

--- ckanext/unaids/react/components/FileInputComponent/src/ResourceForker.js
+++ ckanext/unaids/react/components/FileInputComponent/src/ResourceForker.js
@@ -267,13 +267,9 @@ const ResourceWithDatasetInfoTile = ({ resource, dataset, synced }) => {
                 {resource.id}
             </p>
             {!synced && (
-                <div className="data-out-of-sync-label">
-                    <p>
-                        <span className="label label-warning">
-                            <i className="fa fa-warning"></i>&ensp; Data out of date
-                        </span>
-                    </p>
-                </div>
+                <span className="label label-warning data-out-of-sync-label">
+                    <i className="fa fa-warning"></i>&ensp; Data out of date
+                </span>
             )}
         </div>
     );
