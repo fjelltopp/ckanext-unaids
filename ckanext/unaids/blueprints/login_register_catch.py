661dff4f001a19b5b388bc06e6eb0e70c1e05eb9
Chas Nelson <github@chasnelson.co.uk>
2022-11-29 12:55:39 +0000

    fix: lint

--- ckanext/unaids/blueprints/login_register_catch.py
+++ ckanext/unaids/blueprints/login_register_catch.py
@@ -4,26 +4,22 @@ from flask import Blueprint
 
 log = logging.getLogger(__name__)
 
-login_register_catch = Blueprint(
-    u'login_register_catch',
-    __name__
-)
+login_register_catch = Blueprint("login_register_catch", __name__)
 
 
 def disable_default_login():
-    '''
+    """
     Override and redirect default Login route
-    '''
-    return toolkit.redirect_to('/user/saml2login')
+    """
+    return toolkit.redirect_to("/user/saml2login")
 
 
 def disable_default_register():
-    '''
+    """
     Override and redirect default Register route
-    '''
-    return toolkit.redirect_to('/user/saml2login')
+    """
+    return toolkit.redirect_to("/user/saml2login")
 
-login_register_catch.add_url_rule(
-    u'/user/login', view_func=disable_default_login)
-login_register_catch.add_url_rule(
-    u'/user/register', view_func=disable_default_register)
+
+login_register_catch.add_url_rule("/user/login", view_func=disable_default_login)
+login_register_catch.add_url_rule("/user/register", view_func=disable_default_register)
