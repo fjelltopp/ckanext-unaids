c6e687e04ed3a452cf6cad46de8df4f32cad4e38
Chas Nelson <github@chasnelson.co.uk>
2022-11-29 12:47:26 +0000

    fix: lint

--- ckanext/unaids/tests/test_blueprints.py
+++ ckanext/unaids/tests/test_blueprints.py
@@ -1,20 +1,13 @@
 """Tests for plugin.py."""
 import logging
 import pytest
-import ckan.model
-import ckan.plugins
-from ckanext.unaids.tests.factories import User
 
 log = logging.getLogger(__name__)
 
 
-@pytest.mark.ckan_config('ckan.plugins', 'unaids pages')
-@pytest.mark.usefixtures('with_plugins')
+@pytest.mark.ckan_config("ckan.plugins", "unaids pages")
+@pytest.mark.usefixtures("with_plugins")
 class TestValidateUserProfileBlueprint(object):
-
     def test_annonymous_access_to_index_page(self, app):
-        index_response = app.get(
-            '/',
-            follow_redirects=False
-        )
+        index_response = app.get("/", follow_redirects=False)
         assert index_response.status_code == 200
