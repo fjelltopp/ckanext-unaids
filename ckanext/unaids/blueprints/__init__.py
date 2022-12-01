91690929e87cdf4702ff29f8637538261f2b7e2e
Chas Nelson <github@chasnelson.co.uk>
2022-12-01 12:10:19 +0000

    ADX-897 Some log in links are not pointing out to SSO login and display error message instead (#258)
    * feat: catch login and registration calls
    
    * fix: lint
    
    * fix: resolve code review
    
    * fix: lint

--- ckanext/unaids/blueprints/__init__.py
+++ ckanext/unaids/blueprints/__init__.py
@@ -2,10 +2,12 @@ from ckanext.unaids.blueprints.svg_map_options import svg_map_options
 from ckanext.unaids.blueprints.unaids_dataset_transfer import unaids_dataset_transfer
 from ckanext.unaids.blueprints.user_info_blueprint import user_info_blueprint
 from ckanext.unaids.blueprints.unaids_dataset_releases import unaids_dataset_releases
+from ckanext.unaids.blueprints.login_register_catch import login_register_catch
 
 blueprints = [
     unaids_dataset_transfer,
     user_info_blueprint,
     unaids_dataset_releases,
     svg_map_options,
+    login_register_catch,
 ]
