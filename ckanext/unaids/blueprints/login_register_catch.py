2c37371a8e29103be2aa0acaa562885bdb18b041
Chas Nelson <github@chasnelson.co.uk>
2022-12-01 11:27:51 +0000

    fix: resolve code review

--- ckanext/unaids/blueprints/login_register_catch.py
+++ ckanext/unaids/blueprints/login_register_catch.py
@@ -7,19 +7,11 @@ log = logging.getLogger(__name__)
 login_register_catch = Blueprint("login_register_catch", __name__)
 
 
-def disable_default_login():
+def disable_default_login_register():
     """
     Override and redirect default Login route
     """
     return toolkit.redirect_to("/user/saml2login")
 
-
-def disable_default_register():
-    """
-    Override and redirect default Register route
-    """
-    return toolkit.redirect_to("/user/saml2login")
-
-
-login_register_catch.add_url_rule("/user/login", view_func=disable_default_login)
-login_register_catch.add_url_rule("/user/register", view_func=disable_default_register)
+login_register_catch.add_url_rule("/user/login", view_func=disable_default_login_register)
+login_register_catch.add_url_rule("/user/register", view_func=disable_default_login_register)
