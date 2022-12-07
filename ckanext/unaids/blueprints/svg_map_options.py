bc01b04f4f6d32288f72e23d3e41c531403f3d1e
Jonathan S Berry <jonathan.s.berry@gmail.com>
2022-12-07 18:06:14 +0200

    ADX-858 Map options can filter by type_name

--- ckanext/unaids/blueprints/svg_map_options.py
+++ ckanext/unaids/blueprints/svg_map_options.py
@@ -3,7 +3,7 @@ from collections import defaultdict
 
 import pycountry
 import six
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 
 from ckan.plugins import toolkit
 
@@ -24,15 +24,20 @@ def dataset_count():
 
 
 def map_options():
-    dataset_search = toolkit.get_action("package_search")({}, {"rows": 0, "facet.field": ["geo-location"]})
+    type_name = request.args.get('type_name')
+    search_data_dict = {"rows": 0, "facet.field": ["geo-location"]}
+    if type_name:
+        search_data_dict['q'] = f"type_name:\"{type_name}\""
+    dataset_search = toolkit.get_action("package_search")({}, search_data_dict)
     location_facet = dataset_search['facets']['geo-location']
     values = defaultdict(dataset_count)
     for geo_location, count in six.iteritems(location_facet):
         if geo_location:
             country_code = _country_code_from_location_name(geo_location)
             values[country_code]["count"] = count
-            values[country_code]["link"] = u'/dataset/?geo-location={}'.format(geo_location)
-
+            values[country_code]["link"] = f'/dataset?geo-location={geo_location}'
+            if type_name:
+                values[country_code]["link"] += f"&type_name={type_name}"
     return jsonify({
         "data": {
             "count": {
