046117adad5f8818b4b6f1552b94647c94c2579c
Kamil Forenc <kamil.forenc@gmail.com>
2023-03-24 15:37:45 +0100

    ADX-984 Replace AuthError with ckan's logic.NotAuthorized

--- ckanext/unaids/auth_logic.py
+++ ckanext/unaids/auth_logic.py
@@ -5,6 +5,7 @@ from six.moves.urllib.request import urlopen
 from flask import _request_ctx_stack
 from jose import jwt
 
+from ckan.logic import NotAuthorized
 from ckan.model import Session, User, ApiToken
 
 AUTH0_DOMAIN = config.get('ckanext.unaids.auth0_domain')
@@ -39,23 +40,15 @@ def validate_and_decode_token(encoded):
                 issuer="https://" + AUTH0_DOMAIN + "/"
             )
         except jwt.ExpiredSignatureError:
-            raise AuthError({"code": "token_expired",
-                             "description": "token is expired"}, 401)
+            raise NotAuthorized(message="Token is expired")
         except jwt.JWTClaimsError:
-            raise AuthError({"code": "invalid_claims",
-                             "description":
-                                 "incorrect claims,"
-                                 "please check the audience and issuer"}, 401)
+            raise NotAuthorized(message="Incorrect claims, please check the audience and issuer")
         except Exception:
-            raise AuthError({"code": "invalid_header",
-                             "description":
-                                 "Unable to parse authentication"
-                                 " token."}, 401)
+            raise NotAuthorized(message="Unable to parse authentication token")
 
         _request_ctx_stack.top.current_user = payload
         return payload
-    raise AuthError({"code": "invalid_header",
-                     "description": "Unable to find appropriate key"}, 401)
+    raise NotAuthorized(message="Unable to find appropriate key")
 
 
 def extract_token(encoded_token):
@@ -64,27 +57,14 @@ def extract_token(encoded_token):
     parts = encoded_token.split()
 
     if parts[0].lower() != "bearer":
-        raise AuthError({"code": "invalid_header",
-                        "description":
-                            "Authorization header must start with"
-                            " Bearer"}, 401)
+        raise NotAuthorized(message="Authorization header must start with Bearer")
     elif len(parts) == 1:
-        raise AuthError({"code": "invalid_header",
-                        "description": "Token not found"}, 401)
+        raise NotAuthorized(message="Token not found")
     elif len(parts) > 2:
-        raise AuthError({"code": "invalid_header",
-                        "description":
-                            "Authorization header must be"
-                            " Bearer token"}, 401)
+        raise NotAuthorized(message="Authorization header must be Bearer token")
 
     token = parts[1]
     return token
-
-
-class AuthError(Exception):
-    def __init__(self, error, status_code):
-        self.error = error
-        self.status_code = status_code
 
 
 def verify_required_scope(token):
@@ -96,9 +76,7 @@ def verify_required_scope(token):
             if token_scope == required_scope:
                 return
 
-    raise AuthError({"code": "invalid_header",
-                     "description":
-                         f"Invalid scope. Required: '{required_scope}'"}, 401)
+    raise NotAuthorized(message=f"Invalid scope. Required: '{required_scope}'")
 
 
 def create_new_token(user_id):
@@ -112,9 +90,7 @@ def create_new_token(user_id):
 def get_or_create_ckan_token(decoded_token):
     subject = decoded_token['sub']
     if not subject.startswith("auth0|"):
-        raise AuthError({"code": "invalid_header",
-                         "description":
-                             "Subject is not properly defined"}, 401)
+        raise NotAuthorized(message="Subject claim is not properly defined")
 
     user = find_user(subject)
     api_tokens = Session.query(ApiToken).filter(
@@ -131,13 +107,10 @@ def find_user(subject):
     users = Session.query(User).filter(
         User.plugin_extras["saml2auth", "saml_id"].astext == subject and User.state == 'active'
     ).all()
+
     if len(users) == 0:
-        raise AuthError({"code": "invalid_header",
-                         "description":
-                             "User has not logged into ADR before"}, 401)
+        raise NotAuthorized(message="User has not yet logged into ADR")
     elif len(users) > 1:
-        raise AuthError({"code": "invalid_header",
-                         "description":
-                             "User has more than 1 account, please remove unused accounts"}, 401)
+        raise NotAuthorized(message="User has more than 1 account, please remove unused accounts")
 
     return users[0]
