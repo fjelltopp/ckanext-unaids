35037e28d704f9a8ec1c3f5faf5405580eedfa7c
toavina <toavina@fjelltopp.org>
2023-07-19 17:47:44 +0300

    ADX-1021 with flash message

--- ckanext/unaids/blueprints/ape_data_receiver.py
+++ ckanext/unaids/blueprints/ape_data_receiver.py
@@ -1,27 +1,16 @@
-import json
-
 from flask import Blueprint, redirect, request, flash
-import requests
 from ckan.common import _, g
 from ckan.lib.helpers import url_for
 from ckan.plugins import toolkit
 
-from urllib.parse import urlparse
-
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives import serialization, hashes
-from cryptography.hazmat.primitives.asymmetric import padding
-
 ape_data_receiver = Blueprint("ape_data_receiver", __name__)
-
-
 
 @ape_data_receiver.route('/ape_data_receiver', methods=['GET'])
 def receive():
     if not g.user:
         return toolkit.abort(403, _('You must be logged in to access this page'))
     else:
-        flash(_('User profile successfully saved, you need to log in again to see the changes.'))
+        toolkit.h.flash_success(_('User profile successfully saved, you need to log in again to see the changes.'))
         return redirect(url_for('user.edit', id=g.user))
 
 
