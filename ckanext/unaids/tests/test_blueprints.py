51c3ecd75cd7d84f15494238b8fa9d013996c7f1
Tomasz Sabała <tomek@fjelltopp.org>
2023-10-25 16:14:09 +0200

    ADX-854 Fixup, fixing tests.

--- ckanext/unaids/tests/test_blueprints.py
+++ ckanext/unaids/tests/test_blueprints.py
@@ -5,8 +5,8 @@ import pytest
 log = logging.getLogger(__name__)
 
 
-@pytest.mark.ckan_config("ckan.plugins", "unaids pages")
-@pytest.mark.usefixtures("with_plugins")
+@pytest.mark.ckan_config('ckan.plugins', 'ytp_request unaids pages')
+@pytest.mark.usefixtures('with_plugins')
 class TestValidateUserProfileBlueprint(object):
     def test_annonymous_access_to_index_page(self, app):
         index_response = app.get("/", follow_redirects=False)
