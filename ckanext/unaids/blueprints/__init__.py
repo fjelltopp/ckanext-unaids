7fac569b01717cbd30881bb19617c2223e592067
toavina <toavinahajaina@gmail.com>
2026-01-20 14:02:47 +0300

    Remove login_register_catch blueprint to use default CKAN login
    Disable the SAML2 login redirect to allow using CKAN's built-in
    authentication system instead of forcing users through saml2login.

--- ckanext/unaids/blueprints/__init__.py
+++ ckanext/unaids/blueprints/__init__.py
@@ -2,7 +2,6 @@ from ckanext.unaids.blueprints.svg_map_options import svg_map_options
 from ckanext.unaids.blueprints.unaids_dataset_transfer import unaids_dataset_transfer
 from ckanext.unaids.blueprints.user_info_blueprint import user_info_blueprint
 from ckanext.unaids.blueprints.unaids_dataset_releases import unaids_dataset_releases
-from ckanext.unaids.blueprints.login_register_catch import login_register_catch
 from ckanext.unaids.blueprints.profile_editor_data_receiver import profile_editor_data_receiver
 from ckanext.unaids.blueprints.validate_user_affiliation import validate_user_affiliation
 from ckanext.unaids.blueprints.members_list import members_list
@@ -13,7 +12,6 @@ blueprints = [
     user_info_blueprint,
     unaids_dataset_releases,
     svg_map_options,
-    login_register_catch,
     profile_editor_data_receiver,
     validate_user_affiliation,
     members_list,
