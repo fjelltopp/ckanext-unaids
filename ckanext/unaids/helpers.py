ef663e0742d5d4471e990322122e5ba4f490c870
toavina <toavinahajaina@gmail.com>
2026-01-21 16:13:11 +0300

    Fix validation badge: hide View Error Report link for successful validations

--- ckanext/unaids/helpers.py
+++ ckanext/unaids/helpers.py
@@ -280,21 +280,22 @@ def unaids_get_validation_badge(resource, in_listing=False):
     badge_url = url_for_static_or_external(
         '/images/badges/{}-{}.gif'.format(toolkit.h.lang(), status))
 
-    link_visibility = ""
+    # Hide the "View Error Report" link for successful or unknown validation
+    link_style = ""
     if status in ['success', 'unknown']:
-        link_visibility = 'hidden'
+        link_style = 'style="display:none"'
 
     badge_html = '''
 <a href="{validation_url}" {tags} class="validation-badge">
     <img src="{badge_url}" alt="{alt}" title="{title}"/>
-    <p class="small badge-link {link_visibility}">{badge_link}</p>
+    <p class="small badge-link" {link_style}>{badge_link}</p>
 </a>'''.format(
         validation_url=validation_url,
         tags=tags,
         badge_url=badge_url,
         alt=messages[status],
         title=resource.get('validation_timestamp', ''),
-        link_visibility=link_visibility,
+        link_style=link_style,
         badge_link=_('View Error Report')
     )
 
